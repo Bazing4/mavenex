d1a3ec93a2cc0a0913166c4f71681f9f3095ad9c
Rodrigues <marcoantonio.rodrigu@sie-1222-09.csuci.edu>
2015-10-21 12:57:00 -0700

    first

---
+++ src/main/java/com/mycompany/mavenex/MavenEx.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenex;
+
+/**
+ *
+ * @author marcoantonio.rodrigu
+ */
+public class MavenEx {
+    
+    public static void main (String[] Args){
+        
+       
+       
+    } 
+}
